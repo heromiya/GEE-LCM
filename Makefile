ef73009e826e0a0e0843417127eac53d6bc3c3e1
Hiroyuki Miyazaki <heromiya@heromiya.net>
2020-08-11 15:36:04 +0700

    Apply GRASS for point sampling.

--- Makefile
+++ Makefile
@@ -12,6 +12,25 @@ gt_$(GID).csv: gt_$(GID).tmp
 	for MC_ID in 1 2 3 4; do awk -v mc_id=$$MC_ID '$$3 == mc_id {print rand(),$$0}' $< | sort -k 1 | head -n $(NSAMPLE) | awk 'BEGIN{OFS=","}{print $$2,$$3,$$4}' >> $@; done
 gt_$(GID).vrt:
 	echo "<OGRVRTDataSource><OGRVRTLayer name='gt_$(GID)'><SrcDataSource>gt_$(GID).csv</SrcDataSource><GeometryType>wkbPoint</GeometryType><LayerSRS>WGS84</LayerSRS><GeometryField encoding='PointFromColumns' x='x' y='y'/></OGRVRTLayer></OGRVRTDataSource>" > $@
-gt_$(GID).shp: gt_$(GID).vrt gt_$(GID).csv
+gt_$(GID)_1.gpkg: gt_$(GID).tif #gt_$(GID).vrt gt_$(GID).csv
 	rm -f $@
-	ogr2ogr $@ $<
+	N_MAX=`gdalinfo -hist $< | grep bucket -A 1 | tail -n 1 | sed 's/^ *//g' | cut -f 2 -d " "`; if [ $$N_MAX -lt $(NSAMPLE) ]; then N=$$N_MAX; else N=$(NSAMPLE); fi; printf "r.in.gdal input=gt_$(GID).tif output=gt_$(GID) --overwrite \ng.region raster=gt_$(GID) \nr.null map=gt_$(GID) setnull=2,3,4 \nr.random input=gt_$(GID) npoint=$$N vector=gt_$(GID)_1 --overwrite \nv.out.ogr input=gt_$(GID)_1 output=gt_$(GID)_1.gpkg format=GPKG --overwrite" > $(GID)_1.sh
+	chmod u+x $(GID)_1.sh
+	$(GRASS) -c EPSG:4326 --tmp-location --exec `pwd`/$(GID)_1.sh
+gt_$(GID)_2.gpkg: gt_$(GID).tif
+	rm -f $@
+	N_MAX=`gdalinfo -hist $< | grep bucket -A 1 | tail -n 1 | sed 's/^ *//g' | cut -f 3 -d " "`; if [ $$N_MAX -lt $(NSAMPLE) ]; then N=$$N_MAX; else N=$(NSAMPLE); fi; printf "r.in.gdal input=gt_$(GID).tif output=gt_$(GID) --overwrite \ng.region raster=gt_$(GID) \nr.null map=gt_$(GID) setnull=1,3,4 \nr.random input=gt_$(GID) npoint=$$N vector=gt_$(GID)_2 --overwrite \nv.out.ogr input=gt_$(GID)_2 output=gt_$(GID)_2.gpkg format=GPKG --overwrite" > $(GID)_2.sh
+	chmod u+x $(GID)_2.sh
+	$(GRASS) -c EPSG:4326 --tmp-location --exec `pwd`/$(GID)_2.sh
+gt_$(GID)_3.gpkg: gt_$(GID).tif
+	rm -f $@
+	N_MAX=`gdalinfo -hist $< | grep bucket -A 1 | tail -n 1 | sed 's/^ *//g' | cut -f 4 -d " "`; if [ $$N_MAX -lt $(NSAMPLE) ]; then N=$$N_MAX; else N=$(NSAMPLE); fi; printf "r.in.gdal input=gt_$(GID).tif output=gt_$(GID) --overwrite \ng.region raster=gt_$(GID) \nr.null map=gt_$(GID) setnull=1,2,4 \nr.random input=gt_$(GID) npoint=$$N vector=gt_$(GID)_3 --overwrite \nv.out.ogr input=gt_$(GID)_3 output=gt_$(GID)_3.gpkg format=GPKG --overwrite" > $(GID)_3.sh
+	chmod u+x $(GID)_3.sh
+	$(GRASS) -c EPSG:4326 --tmp-location --exec `pwd`/$(GID)_3.sh
+gt_$(GID)_4.gpkg: gt_$(GID).tif
+	rm -f $@
+	N_MAX=`gdalinfo -hist $< | grep bucket -A 1 | tail -n 1 | sed 's/^ *//g' | cut -f 5 -d " "`; if [ $$N_MAX -lt $(NSAMPLE) ]; then N=$$N_MAX; else N=$(NSAMPLE); fi; printf "r.in.gdal input=gt_$(GID).tif output=gt_$(GID) --overwrite \ng.region raster=gt_$(GID) \nr.null map=gt_$(GID) setnull=1,2,3 \nr.random input=gt_$(GID) npoint=$$N vector=gt_$(GID)_4 --overwrite \nv.out.ogr input=gt_$(GID)_4 output=gt_$(GID)_4.gpkg format=GPKG --overwrite" > $(GID)_4.sh
+	chmod u+x $(GID)_4.sh
+	$(GRASS) -c EPSG:4326 --tmp-location --exec `pwd`/$(GID)_4.sh
+
+#	ogr2ogr $@ $<
--- merge-trainingdata.sh
+++ merge-trainingdata.sh
@@ -2,22 +2,26 @@
 
 export NSAMPLE=1000 # Number of sample per class in a scene.
 
-# For Windows
-export PYTHONPATH=/c/OSGeo4W64/apps/Python37/Lib
-export PATH=$PATH:/c/OSGeo4W64/bin
+# For Windows with OSGeo4W64 and Git Bash
+if [ $OS = 'Windows_NT' ]; then 
+    export PYTHONPATH=/c/OSGeo4W64/apps/Python37/Lib
+    export PATH=$PATH:/c/OSGeo4W64/bin
+    export PATH=$PATH:`pwd`
+    export GRASS=/c/OSGeo4W64/bin/grass78.bat
+else
+    export GRASS=grass78
+fi
 
-export PATH=$PATH:`pwd`
 export WORKDIR=$(mktemp -d)
 export OUTDB=$WORKDIR/tmp.sqlite
 SCPDIR="SCP-shapefiles"
 #:<<'#EOF'
 
 rm -f $OUTDB
-
 SQL=$WORKDIR/tmp.sql
 echo "DELETE FROM geometry_columns WHERE f_table_name = 'gt' OR  f_table_name = 'pt_gt'; DROP TABLE IF EXISTS gt; CREATE TABLE gt AS" > $SQL
 
-for SHP in `ls $SCPDIR | grep shp$ | sed 's/\.shp//g'`; do
+for SHP in `ls $SCPDIR | grep shp$ | sed 's/\.shp//g'; do
     TBL=$(echo $SHP | sed 's/-.*//g' | tr A-Z a-z | sed 's/-//g')
     GID=$(echo $SHP | sed -e "s/\(L.\{24\}\).*/\1/g; s/-//g")
     PROJ="$(cat $SCPDIR/$SHP.prj)"
@@ -35,22 +39,28 @@ spatialite -silent $OUTDB "INSERT INTO gt (geometry,mc_id,gid) SELECT ST_Transfo
 
 
 mkdir -p pointize
-for GID in `spatialite $OUTDB "SELECT gid from gt group by gid;" | sed 's/\r//g'`; do
-#f_pointize() {
-#    export GID=$1
-    export GID
-    WORKDIR_P=$(mktemp -d)
-    make -C $WORKDIR_P -f $PWD/Makefile gt_$GID.shp
-    mv $WORKDIR_P/gt_$GID.* pointize/
-    rm -rf $WORKDIR_P
-#}
-#export -f f_pointize
-#parallel --bar f_pointize ::: `spatialite $OUTDB "SELECT gid from gt group by gid;"`
-done
+f_pointize() {
+    export GID=$1
+    export WORKDIR_P=$(mktemp -d)
+    make -C pointize -f $PWD/Makefile gt_${GID}_1.gpkg gt_${GID}_2.gpkg gt_${GID}_3.gpkg gt_${GID}_4.gpkg
+    #mv $WORKDIR_P/gt_$GID.* pointize/
+#    rm -rf $WORKDIR_P
+}
+export -f f_pointize
+
+parallel --version
+if [ $? -eq 0 ]; then
+    parallel --bar f_pointize ::: `spatialite $OUTDB "SELECT gid from gt group by gid;"`
+else 
+    for GID in `spatialite $OUTDB "SELECT gid from gt group by gid;" | sed 's/\r//g'`; do 
+        f_pointize $GID
+    done
+fi
+
 #EOF
 
-
 rm -f gt-pt.*
-for LAYER in `ogrinfo pointize | grep Point | awk '{print $2}'`; do
-    ogr2ogr -append -sql "SELECT '`echo $LAYER | sed 's/gt_//g'`' as gid, cast(mc_id as integer) as class, cast(`echo $LAYER | sed 's/gt_L[CETM][0-9]\{2\}_[A-Z0-9]\{4\}_[0-9]\{6\}_\([0-9]\{4\}\)[0-9]\{4\}/\1/; s/gt_L[CET][0-9]\{7\}\([0-9]\{4\}\).*/\1/; s/gt_L[CET][0-9]._\([0-9]\{4\}\).*/\1/;'` as integer) AS year from $LAYER" gt-pt.shp pointize $LAYER
+for GPKG in pointize/*.gpkg ; do
+    LAYER=$(ogrinfo $GPKG | grep Point | awk '{print $2}')
+    ogr2ogr -append -sql "SELECT geom, '`echo $LAYER | sed 's/gt_//g'`' as gid, cast(value as integer) as class, cast(`echo $LAYER | sed 's/gt_L[CETM][0-9]\{2\}_[A-Z0-9]\{4\}_[0-9]\{6\}_\([0-9]\{4\}\)[0-9]\{4\}/\1/; s/gt_L[CET][0-9]\{7\}\([0-9]\{4\}\).*/\1/; s/gt_L[CET][0-9]._\([0-9]\{4\}\).*/\1/;'` as integer) AS year from $LAYER" gt-pt.shp $GPKG $LAYER
 done
